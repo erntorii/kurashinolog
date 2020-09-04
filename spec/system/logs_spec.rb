90b50009c49a3c6749b02eea3bb96e85d212e1ca
erntorii <ern4273@gmail.com>
2020-09-04 14:33:18 +0900

    Add full_title helper method

--- spec/system/logs_spec.rb
+++ spec/system/logs_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Logs', type: :system do
   it 'get home' do
     visit root_path
-    expect(page).to have_title 'index - 暮らしのログ'
+    expect(page).to have_title '暮らしのログ'
   end
 
   it 'get about' do
-    visit root_path
-    expect(page).to have_title 'index - 暮らしのログ'
+    visit about_path
+    expect(page).to have_title 'about - 暮らしのログ'
   end
 end
