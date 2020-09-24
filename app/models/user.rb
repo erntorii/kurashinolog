2a353cd1a0c06a8fe48934bc8d500404aa70c043
erntorii <ern4273@gmail.com>
2020-09-24 19:34:09 +0900

    Fix password edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
 
   def self.guest
     find_or_create_by!(name: 'ゲスト', email: 'guest@example.com') do |user|
