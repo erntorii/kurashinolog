fda5756c17b316cc82883ca3b7a4c98ad1e0e309
erntorii <ern4273@gmail.com>
2020-10-26 12:06:59 +0900

    Cut password_confirmation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  # ユーザー編集時のパスワード入力を省略する
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   def after_update_path_for(_resource)
     edit_user_registration_path
   end
