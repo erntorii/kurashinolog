4e4a14a4594d845bb2f6d94a4777b77bc469b14f
erntorii <ern4273@gmail.com>
2020-10-28 20:26:34 +0900

    Add :dependent option

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,6 @@
 class Log < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 40 }
   validates :content, presence: true
