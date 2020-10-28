fab3c0951dc030f98e783f98807491fadbe1a95a
erntorii <ern4273@gmail.com>
2020-10-28 20:19:41 +0900

    Add log model validates

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,6 @@
 class Log < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :logs
   has_one_attached :image
   validates :name, presence: true, length: { maximum: 20 }
   validates :profile, length: { maximum: 200 }
