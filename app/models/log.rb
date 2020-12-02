f7b0b38867e610fd9643539ee545a49dae034b79
erntorii <ern4273@gmail.com>
2020-12-02 19:38:27 +0900

    Fix

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,6 +2,6 @@ class Log < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 40 }
+  validates :title, presence: true, length: { maximum: 60 }
   validates :content, presence: true
 end
--- spec/system/logs_spec.rb
+++ spec/system/logs_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Logs', type: :system do
-  it 'get home' do
-    visit root_path
-    expect(page).to have_title '暮らしのログ'
-  end
+  describe 'ページタイトル' do
+    it 'get home' do
+      visit root_path
+      expect(page).to have_title '暮らしのログ'
+    end
 
-  it 'get about' do
-    visit about_path
-    expect(page).to have_title 'about - 暮らしのログ'
+    it 'get about' do
+      visit about_path
+      expect(page).to have_title 'about - 暮らしのログ'
+    end
   end
 end
